44d78e79ad7abc77a8df5ade1addbf05bca029b5
marwan21ahmed <marwan21ahmed@gmail.com>
2024-02-12 22:16:06 +0200

    done customer model

---
+++ HostelReservation.DbContext/Configration/CustomerConfigration.cs
@@ -0,0 +1,25 @@
+﻿using HostelReservation.Model;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelReservation.Context.Configration
+{
+    internal class CustomerConfigration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(s => s.ID);
+            builder.Property(c => c.Name).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(c => c.Phone).HasColumnType("intger").IsRequired();
+            builder.Property(c => c.City).HasColumnType("varchar(50)").IsRequired();
+
+
+        }
+    
+    }
+}
---
+++ HostelReservation.Model/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelReservation.Model
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string? City { get; set; }
+        public int Phone { get; set; }
+    }
+}
