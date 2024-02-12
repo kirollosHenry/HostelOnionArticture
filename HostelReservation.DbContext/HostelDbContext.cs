96ac3fd43fb752aade57cff0c02a4489dfd0f0f4
AbanoubGirGis <abanoubgirgis677@gmail.com>
2024-02-12 23:07:04 +0200

    Room Model

--- HostelReservation.DbContext/HostelDbContext.cs
+++ HostelReservation.DbContext/HostelDbContext.cs
@@ -12,14 +12,13 @@ namespace HostelReservation.Context
 {
     public class HostelDbContext : DbContext
     {
+        public DbSet<Room> Rooms { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=HostelDB;Integrated Security=True;Encrypt=false;Trust Server Certificate=True;");
             base.OnConfiguring(optionsBuilder);
-
-
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ HostelReservation.Model/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelReservation.Model
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+
+        public int RoomRates {  get; set; }
+
+        public int RoomNumberBeds { get; set; }
+
+        public char RoomStatus { get; set; }
+
+        //public Hotel hotel { get; set; }
+    }
+}
