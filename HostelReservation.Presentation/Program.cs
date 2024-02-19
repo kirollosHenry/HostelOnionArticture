691ae6ec75ecf6b79a038c5a54677254459251b0
AbanoubGirGis <abanoubgirgis677@gmail.com>
2024-02-19 18:49:22 +0200

    Update Login

--- HostelReservation.Presentation/Program.cs
+++ HostelReservation.Presentation/Program.cs
@@ -13,7 +13,6 @@ namespace HostelReservation.Presentation
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new Resaption());
             Application.Run(new LoginForm());
         }
     }
